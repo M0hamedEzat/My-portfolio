c1bfa81bea40653a20ec851b8851acbf36da559e
Mohamed <ezatm1083@gmail.com>
2023-02-12 21:18:19 +0200

    first beta version of the site

--- js/app.js
+++ js/app.js
@@ -39,3 +39,20 @@ $(document).ready(function () {
     }
   });
 });
+const sections = document.querySelectorAll("section");
+
+window.onscroll = function () {
+  sections.forEach(function (active) {
+    // loops for all the sections
+    if (
+      active.getBoundingClientRect().top >= -500 &&
+      active.getBoundingClientRect().top <= 450
+    ) {
+      //Add class
+      active.classList.add("your-active-class"); // active the class, when the if statment is true
+    } else {
+      // Remove class
+      active.classList.remove("your-active-class"); // remove active class, when one of the if statment is false
+    }
+  });
+};
